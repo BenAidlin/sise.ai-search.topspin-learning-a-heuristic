bbc0478b7a9d2bc0f69ede2be87d0882cd100540
Ben Aidlin <ben.aidlin@intel.com>
2023-05-11 19:13:21 +0300

    implement top spin state

--- topspin.py
+++ topspin.py
@@ -1,14 +1,30 @@
-
 class TopSpinState:
 
     def __init__(self, state, k=4):
-        pass
+        self.state = state
+        self.n = len(state)
+        self.k = k
+        self.father_state = None
 
     def is_goal(self):
-        pass
+        return self.state == list(range(1, self.n + 1))
 
     def get_state_as_list(self):
-        pass
+        return self.state
 
     def get_neighbors(self):
-        pass
+        neighbors = []
+        # move the outer ring clockwise
+        neighbors.append(TopSpinState(self.state[-1:] + self.state[:-1], self.k))
+        # move the outer ring counter-clockwise
+        neighbors.append(TopSpinState(self.state[1:] + self.state[:1], self.k))
+        # flip the first k disks on the inner ring
+        if self.k > 1:
+            flipped = self.state[:self.k][::-1] + self.state[self.k:]
+            neighbors.append(TopSpinState(flipped, self.k))
+        for n in neighbors:
+            n.father_state = self
+        return neighbors
+    
+    def get_father_state(self):
+        return self.father_state
