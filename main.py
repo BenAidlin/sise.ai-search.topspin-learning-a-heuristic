306d4bfd5a0f38f90cbd037826b1d7dd04183178
Ben Aidlin <ben.aidlin@intel.com>
2023-05-27 18:42:41 +0300

    upgrade main

--- main.py
+++ main.py
@@ -5,15 +5,45 @@ from priorities import f_priority
 from search import search
 from topspin import TopSpinState
 
+import time
+# timeit wrapper to use in code
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"took {elapsed_time:.2f} seconds to execute.")
+        return result
+    return wrapper
+
+@timeit
+def search_with_time(start: TopSpinState, heuristic: any, priority: function, print_states: bool):
+    path, expansions = search(start, priority, heuristic.get_h_value)
+    if path is not None:
+        print(f'expansions: {expansions}')
+        if(print_states):
+            for vertex in path:
+                print(vertex)
+    else:
+        print("unsolvable")
+
 instance_1 = [1, 7, 10, 3, 6, 9, 5, 8, 2, 4, 11]  # easy instance
 instance_2 = [1, 5, 11, 2, 6, 3, 9, 4, 10, 7, 8]  # hard instance
 
-start = TopSpinState(instance_1, 4)
-heuristic = AdvanceHeuristic(11, 4)
-path, expansions = search(start, f_priority, heuristic.get_h_value)
-if path is not None:
-    print(expansions)
-    for vertex in path:
-        print(vertex)
-else:
-    print("unsolvable")
+easy_instance_start = TopSpinState(instance_1, 4)
+hard_instance_start = TopSpinState(instance_2, 4)
+base_heuristic = BaseHeuristic(11, 4)
+advanced_heuristic = AdvanceHeuristic(11, 4)
+    
+print("-------------easy instance base heuristic----------------")
+search_with_time(easy_instance_start, base_heuristic, f_priority, False)
+
+print("-------------easy instance advanced heuristic----------------")
+search_with_time(easy_instance_start, advanced_heuristic, f_priority, False)
+
+print("-------------hard instance base heuristic----------------")
+search_with_time(hard_instance_start, base_heuristic, f_priority, False)
+
+print("-------------hard instance advanced heuristic----------------")
+search_with_time(hard_instance_start, advanced_heuristic, f_priority, False)
