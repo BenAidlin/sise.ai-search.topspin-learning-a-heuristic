9a46db1d970772bdb4b583b946ee06717c9ddc17
Ben Aidlin <ben.aidlin@intel.com>
2023-05-27 10:45:02 +0300

    use advanced heuristic in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-from heuristics import BaseHeuristic
+from heuristics import BaseHeuristic, AdvanceHeuristic
 from priorities import f_priority
 from search import search
 from topspin import TopSpinState
@@ -9,7 +9,7 @@ instance_1 = [1, 7, 10, 3, 6, 9, 5, 8, 2, 4, 11]  # easy instance
 instance_2 = [1, 5, 11, 2, 6, 3, 9, 4, 10, 7, 8]  # hard instance
 
 start = TopSpinState(instance_1, 4)
-heuristic = BaseHeuristic(11, 4)
+heuristic = AdvanceHeuristic(11, 4)
 path, expansions = search(start, f_priority, heuristic.get_h_value)
 if path is not None:
     print(expansions)
