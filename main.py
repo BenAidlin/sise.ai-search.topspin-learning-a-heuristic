38a8960f09c9426216c4c0f7242265f5aac4af97
Ben Aidlin <ben.aidlin@intel.com>
2023-05-30 16:23:51 +0300

    write the code to conditionally train and run instances

--- main.py
+++ main.py
@@ -2,70 +2,80 @@ import random
 from heuristic_trainer import HeuristicTrainer
 
 from heuristics import BaseHeuristic, AdvanceHeuristic, LearnedHeuristic
-from priorities import f_priority
+from priorities import f_priority, h_priority, fw_priority
 from search import search
 from topspin import TopSpinState
 
 import time
-# timeit wrapper to use in code
-def timeit(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"took {elapsed_time:.2f} seconds to execute.")
-        return result
-    return wrapper
 
-@timeit
-def search_with_time(start: TopSpinState, heuristic: any, priority, print_states: bool):
+training_needed = False
+instances_to_run = 50
+base_heuristic = BaseHeuristic(11, 4)
+advanced_heuristic = AdvanceHeuristic(11, 4)
+learned_heuristic = LearnedHeuristic(11, 4)
+try:
+    learned_heuristic.load_model() # load last learned heuristic
+except:
+    print('no current model to load, will start from random weights')
+
+class SearchResult:
+    def __init__(self, runtime, path_length, expansions) -> None:
+        self.runtime = runtime
+        self.path_length = path_length
+        self.expansions = expansions
+
+def search_and_record_performance(start: TopSpinState, heuristic: any, priority, print_states: bool) -> SearchResult:
+    start_time = time.time()
     path, expansions = search(start, priority, heuristic.get_h_value)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     if path is not None:
-        print(f'expansions: {expansions}')
         path_length = 0        
         for vertex in path:
             if(print_states):
                 print(vertex)
             path_length+=1
-        print(f'path length: {path_length}')
+        return SearchResult(elapsed_time, path_length, expansions)
     else:
         print("unsolvable")
+        return None
 
-instance_1 = [1, 7, 10, 3, 6, 9, 5, 8, 2, 4, 11]  # easy instance
-instance_2 = [1, 5, 11, 2, 6, 3, 9, 4, 10, 7, 8]  # hard instance
+def generate_scrambled_state() -> TopSpinState:    
+    how_many_scrambles = random.randint(1, 10000)
+    print(f'Generating scrambled state with {how_many_scrambles} scrambles')
+    return HeuristicTrainer(learned_heuristic).generate_scrambled_state(how_many_scrambles)
+    
 
-easy_instance_start = TopSpinState(instance_1, 4)
-hard_instance_start = TopSpinState(instance_2, 4)
-base_heuristic = BaseHeuristic(11, 4)
-advanced_heuristic = AdvanceHeuristic(11, 4)
 
-# check given instances with base and advanced heuristic    
-# print("-------------easy instance base heuristic----------------")
-# search_with_time(easy_instance_start, base_heuristic, f_priority, False)
+def train_learned_heuristic(learned_heuristic: LearnedHeuristic, times: int, max_steps: int, epochs: int):
+    heuristic_trainer = HeuristicTrainer(learned_heuristic) 
+    # train model
+    print("-------------training learned heuristic----------------")
+    for i in range(times):
+        print(f"-------------training number {i} done----------------")
+        heuristic_trainer.train(max_steps=max_steps, epochs=epochs)
+    learned_heuristic.save_model()
 
-# print("-------------easy instance advanced heuristic----------------")
-# search_with_time(easy_instance_start, advanced_heuristic, f_priority, False)
 
-# print("-------------hard instance base heuristic----------------")
-# search_with_time(hard_instance_start, base_heuristic, f_priority, False)
+if training_needed:
+    train_learned_heuristic(learned_heuristic, 100, 1000, 100)
 
-# print("-------------hard instance advanced heuristic----------------")
-# search_with_time(hard_instance_start, advanced_heuristic, f_priority, False)
 
-learned_heuristic = LearnedHeuristic(11, 4)
-heuristic_trainer = HeuristicTrainer(learned_heuristic)
+priorities = [('A*', f_priority), ('GBFS', h_priority), ('WA*', fw_priority)]
+heuristics = [('advanced', advanced_heuristic), ('basic', base_heuristic), ('learned', learned_heuristic),]
+instances = [generate_scrambled_state() for _ in range(instances_to_run)]
 
-# train model
-print("-------------training learned heuristic----------------")
-for i in range(10):
-    print(f"-------------training number {i} done----------------")
-    heuristic_trainer.train(max_steps=1000, epochs=100)
-learned_heuristic.save_model()
-
-# try model
-learned_heuristic.load_model()
-print("-------------easy instance learned heuristic----------------")
-search_with_time(easy_instance_start, learned_heuristic, f_priority, False)
-print("-------------hard instance learned heuristic----------------")
-search_with_time(hard_instance_start, learned_heuristic, f_priority, False)
+print(f'running {instances_to_run} of each variation')
+for heuristic in heuristics:        
+    for priority in priorities:        
+        priority_heuristic_results = []
+        for instance in enumerate(instances):
+            print(f'running {priority[0]} with {heuristic[0]} and instance number {instance[0]}')
+            priority_heuristic_results.append(search_and_record_performance(instance[1], heuristic[1], priority[1], False))
+        runtime_results = [sr.runtime for sr in priority_heuristic_results if sr is not None] 
+        path_length_results = [sr.path_length for sr in priority_heuristic_results if sr is not None] 
+        expansions_results = [sr.expansions for sr in priority_heuristic_results if sr is not None] 
+        print(f'{priority[0]} with {heuristic[0]} resulted in ' + 
+              f'avg runtime: {sum(runtime_results) / len(runtime_results)},' + 
+              f'avg path_length: {sum(path_length_results) / len(path_length_results)},' + 
+              f'avg expansions: {sum(expansions_results) / len(expansions_results)}')
